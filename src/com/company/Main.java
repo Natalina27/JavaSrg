e8035ea1e9f4a3e9fea3396151048a780db01b83
Natalya <natalyamyunster@yahoo.com>
2019-08-12 11:05:53 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,44 @@
+
+package com.company;
+
+//public class Main{
+//
+//    public static void main(String[] args) {
+//
+//        int a = 2_000_000_000;
+//        long b = 2;
+//
+//        int  z = (int)(a * b);
+//        System.out.print(z);
+//        System.out.print(a / b);
+//        System.out.print('\n');
+//      // write your code here
+//        System.out.print("Hello World!");
+//    }
+//}
+
+
+900\;//HOMEWORK 1 JAVA
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+//        for (int i = 40; i <= 60; i++) {
+//            if ( i % 4 == 0) {
+//                System.out.print(i);
+//                System.out.print('\n');
+//
+//            }
+
+        String s = "Перестановочный алгоритм быстрого действия";
+        for (int i = 0; i <= s.lang.String; i++) {
+            if (s[i] == "o") {
+                System.out.print(s[i]);
+                System.out.print('\n');
+
+            }
+        }
+    }
+}
